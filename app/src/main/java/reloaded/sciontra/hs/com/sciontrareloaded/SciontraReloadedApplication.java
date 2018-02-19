41a48fe08185f79f9694290d84aa5eac94d82fd7
Rishabh <rishabhjain.engineer@gmail.com>
2018-02-19 13:22:19 +0530

    Deployed some of the basic libraries

---
+++ app/src/main/java/reloaded/sciontra/hs/com/sciontrareloaded/SciontraReloadedApplication.java
@@ -0,0 +1,23 @@
+package reloaded.sciontra.hs.com.sciontrareloaded;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by rishabh on 19/2/18.
+ */
+
+public class SciontraReloadedApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(LeakCanary.isInAnalyzerProcess(this)){
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
