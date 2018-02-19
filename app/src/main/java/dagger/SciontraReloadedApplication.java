6fed6e878acd2c0e479dfd39e85e314f21489739
ayazAndroid <ayazhealthscion.com>
2018-02-19 14:35:54 +0530

    Fixed Issues

--- app/src/main/java/dagger/SciontraReloadedApplication.java
+++ app/src/main/java/dagger/SciontraReloadedApplication.java
@@ -1,8 +1,12 @@
-package reloaded.sciontra.hs.com.sciontrareloaded;
+package dagger;
 
 import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
+
+import dagger.ApiComponent;
+import dagger.ApiModule;
+import dagger.AppModule;
 
 /**
  * Created by rishabh on 19/2/18.
@@ -10,14 +14,21 @@ import com.squareup.leakcanary.LeakCanary;
 
 public class SciontraReloadedApplication extends Application {
 
+    private ApiComponent mApiComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        if(LeakCanary.isInAnalyzerProcess(this)){
+        if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
             // You should not init your app in this process.
             return;
         }
         LeakCanary.install(this);
+        mApiComponent = DaggerApiComponent.builder().appModule(new AppModule(this)).apiModule(new ApiModule("https://simplifiedcoding.net/demos/")).build();
+    }
+
+    public ApiComponent getNetComponent() {
+        return mApiComponent;
     }
 }
